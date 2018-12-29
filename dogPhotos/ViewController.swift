04d9482a79e951774b803a1e60be662082dd5ec9
Destiny Hochhalter <destinyhochhalter@destinys-macbook-pro.local>
2018-12-29 13:24:23 -0800

    Initial Commit

---
+++ dogPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dogPhotos
+//
+//  Created by Destiny Hochhalter on 12/29/18.
+//  Copyright © 2018 Destiny Hochhalter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
